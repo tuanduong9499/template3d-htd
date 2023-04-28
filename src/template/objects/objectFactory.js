d0fba132c8c4b91a2c0e2373b08af964e7a1e836
tuanduong9499 <tuanduong9499@gmail.com>
2023-04-28 14:01:12 +0700

    + createModel of objectFactory

--- src/template/objects/objectFactory.js
+++ src/template/objects/objectFactory.js
@@ -2,7 +2,7 @@ import { Entity } from "playcanvas";
 import { AssetLoader } from "../../assetLoader/assetLoader";
 
 export class ObjectFactory {
-  static createModel(modelAsset){
+  static createModel(modelAsset, animationAssets){
     let entity = new Entity();
     entity.addComponent("model", {
       asset: AssetLoader.getAssetByKey(modelAsset)

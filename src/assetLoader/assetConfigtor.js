beea63d5f6fe5562272aae9185f30dd3ec730d3c
tuanduong9499 <tuanduong9499@gmail.com>
2023-04-28 14:20:27 +0700

    + config material character

--- src/assetLoader/assetConfigtor.js
+++ src/assetLoader/assetConfigtor.js
@@ -1,9 +1,10 @@
-import { Texture } from "playcanvas";
+import { StandardMaterial, Texture } from "playcanvas";
 import { AssetLoader } from "./assetLoader";
 
 export class AssetConfig {
   static config(app){
-    this._configSkyboxCubemap(app)
+    this._configSkyboxCubemap(app);
+    this._configMaterialCharacter();
   }
 
   static _configSkyboxCubemap(app) {
@@ -20,5 +21,23 @@ export class AssetConfig {
     });
     cmSkybox.setSource(textures.map((texture) => texture.resource.getSource()));
     AssetLoader.registerAsset(cmSkybox, "cm_skybox", "cubemap");
+  }
+
+  static _configMaterialCharacter(){
+    let material1 = new StandardMaterial();
+    material1.diffuseMap = AssetLoader.getAssetByKey("Boy01_diffuse").resource;
+
+    let material2 = new StandardMaterial();
+    material2.diffuseMap = AssetLoader.getAssetByKey("Boy01_FacialAnimMap").resource;
+
+    let model = AssetLoader.getAssetByKey("character").resource;
+    console.log(model.meshInstances)
+    model.meshInstances.forEach((mat, index) => {
+      if(index === 0){
+        mat.material = material1;
+      }else{
+        mat.material = material2
+      }
+    });
   }
 }

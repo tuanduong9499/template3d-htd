7ef71c6bfedd7ae9d3ad0f0e9a0054a41a19da37
tuanduong9499 <tuanduong9499@gmail.com>
2023-04-28 14:01:24 +0700

    + character

--- src/objects/character/character.js
+++ src/objects/character/character.js
@@ -1,9 +1,34 @@
 import { Entity } from "playcanvas";
 import { ObjectFactory } from "../../template/objects/objectFactory";
+import { AssetLoader } from "../../assetLoader/assetLoader";
 
 export class Character extends Entity {
   constructor(){
     super();
-    this.addChild(ObjectFactory.createModel("character"));
+    this.isEnable = false;
+    this._createModel();
+    this._initAnimation();
+  }
+
+  _createModel(){
+    this.character = ObjectFactory.createModel("character");
+    this.addChild(this.character);
+  }
+
+  _initAnimation(){
+    this.character.addComponent("animation", {
+      assets : [AssetLoader.getAssetByKey("anim_idle")],
+      activate : true,
+      loop : true,
+    });
+
+    document.addEventListener("pointerdown", () => {
+      this.isEnable = !this.isEnable;
+      if(this.isEnable){
+        this.character.animation.enabled = true;
+      }else{
+        this.character.animation.enabled = false;
+      }
+    });
   }
 }
